738ca2b3cc524242d872264fc14b53d6271d90fd
Sachin Somia <sachinsomia@gmail.com>
2024-12-10 20:59:00 +0000

    feat: day3 Part B

--- packages/2024/day3/day03.go
+++ packages/2024/day3/day03.go
@@ -54,3 +54,66 @@ func SolvePartA(filename string) int {
 
 	return result
 }
+
+func SolvePartB(filename string) int {
+	input := utils.OpenFile(filename)
+	lines := parseInput(input)
+
+	re, err := regexp.Compile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	if err != nil {
+		log.Fatalf("regexp failed to compile, %v\n", err)
+	}
+	// numRe, err := regexp.Compile(`mul\((\d+),(\d+)\)`)
+	// if err != nil {
+	// 	log.Fatalf("regexp failed to compile, %v\n", err)
+	// }
+
+	muls := make([]string, 0)
+	m := make([][]int, 0)
+	for _, line := range lines {
+		matches := re.FindAllString(line, -1)
+		muls = append(muls, matches...)
+	}
+
+	fmt.Printf("MULS:\n %#v\n", muls)
+	enabled := true
+	for _, mul := range muls {
+		if mul == "do()" {
+			enabled = true
+			continue
+		}
+		if mul == "don't()" {
+			enabled = false
+			continue
+		}
+
+		if !enabled {
+			continue
+		}
+		matchGroup := strings.Split(mul[4:len(mul)-1], ",")
+		fmt.Printf("matchGroup:%s\n", matchGroup)
+		n := make([]int, 0)
+		num1s := string(matchGroup[0])
+		num1, err := strconv.Atoi(num1s)
+		if err != nil {
+			fmt.Printf("Failed to convert num: %v\n", err)
+		}
+		n = append(n, num1)
+
+		num2s := string(matchGroup[1])
+		num2, err := strconv.Atoi(num2s)
+		if err != nil {
+			fmt.Printf("Failed to convert num: %v\n", err)
+		}
+		n = append(n, num2)
+
+		m = append(m, n)
+	}
+
+	result := 0
+	for _, mm := range m {
+		result = result + (mm[0] * mm[1])
+	}
+
+	return result
+}
--- packages/2024/day3/day03_test.go
+++ packages/2024/day3/day03_test.go
@@ -1,11 +1,21 @@
 package day3
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPartA(t *testing.T) {
 	expected := 161
 	result := SolvePartA("../../../input/2024/day03.txt")
 	if result != expected {
 		t.Errorf("Part A failed, Expected: %v, Found: %v\n", expected, result)
+	}
+}
+
+func TestPartB(t *testing.T) {
+	expected := 48
+	result := SolvePartB("../../../input/2024/day03.txt")
+	if result != expected {
+		t.Errorf("Part B failed, Expected: %v, Found: %v\n", expected, result)
 	}
 }
